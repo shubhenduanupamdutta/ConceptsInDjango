a4370ba8822238c40a00ab8ab155b113f43a5a66
ShubhenduADutta <shubhendu@flexli.in>
2023-11-18 13:45:42 +0530

    Delete redundant import

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,6 +1,5 @@
 # from django.shortcuts import render
 from typing import Any
-from django.core.paginator import Paginator
 from django.db.models.query import QuerySet
 from django.views.generic import ListView
 from .models import Movie
